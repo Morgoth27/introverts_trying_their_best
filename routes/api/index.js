194aa43e2e6693369bc9e359616f9a335cc7741c
Tiffany <leopardmitai@gmail.com>
2022-03-27 13:31:00 -0700

    Updated api/index.js with the additional routes. Troubleshooting

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const thoughtsRoutes = require ('./thoughtsRoutes');
-// const followersRoutes = require ('./followersRoutes')
+const thoughtsRoutes = require('./thoughtsRoutes');
+const followersRoutes = require('./followersRoutes');
+const reactionRoutes = require('./reactionRoutes')
 
-router.use('/users', userRoutes);
-router.use('/thoughts', thoughtsRoutes);
+router.use('/users', userRoutes, followersRoutes);
+router.use('/thoughts', thoughtsRoutes, reactionRoutes);
 // router.use('/followers', followersRoutes)
 
-module.exports = router
+module.exports = router;
